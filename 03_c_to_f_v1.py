ef0cf6791ada71385554996dd7ac2d9213d9ec24
Jamman72341 <52226470+Jamman72341@users.noreply.github.com>
2020-02-26 09:50:52 +1300

    updated

--- 03_c_to_f_v1.py
+++ 03_c_to_f_v1.py
@@ -11,3 +11,9 @@ def to_f(from_c):
 temperatures = [0, 40, 100]
 converted = []
 
+for item in temperatures:
+    answer = to_f(item)
+    ans_statement = "{} degrees C is {} degrees F".format(item, answer)
+    converted.append(ans_statement)
+
+print(converted)
